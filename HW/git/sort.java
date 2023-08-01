e58cc1a7ff47d90a2ca9a2494b189f44fa01a32c
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-08-01 08:02:44 +0300

    add file

---
+++ HW/git/sort.java
@@ -0,0 +1,48 @@
+package HW.git;
+
+public class sort {
+    public static void main(String[] args) {
+        int[] array = new int[] { 1, 5, 2, 7, 4, 6, 7, 4, 3, 8, 4, 9 };
+
+        sort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    static void sort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest])
+            largest = leftChild;
+
+        if (rightChild < heapSize && array[rightChild] > array[largest])
+            largest = rightChild;
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+            
+            heapify(array, heapSize, largest);
+        }
+
+    }
+}
