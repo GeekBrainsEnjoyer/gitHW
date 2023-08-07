65921b845de44eaf7b2a39f580d50c62e036d657
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-08-07 06:40:11 +0300

    no no

--- HW/git/sort.java
+++ HW/git/sort.java
@@ -1,48 +1,48 @@
-package HW.git;
+// package HW.git;
 
-public class sort {
-    // public static void main(String[] args) {
-    //     int[] array = new int[] { 1, 5, 2, 7, 4, 6, 7, 4, 3, 8, 4, 9 };
+// public class sort {
+//     // public static void main(String[] args) {
+//     //     int[] array = new int[] { 1, 5, 2, 7, 4, 6, 7, 4, 3, 8, 4, 9 };
 
-    //     sort(array);
+//     //     sort(array);
 
-    //     for (int i = 0; i < array.length; i++) {
-    //         System.out.print(array[i] + " ");
-    //     }
-    // }
+//     //     for (int i = 0; i < array.length; i++) {
+//     //         System.out.print(array[i] + " ");
+//     //     }
+//     // }
 
-    // static void sort(int[] array) {
-    //     for (int i = array.length / 2 - 1; i >= 0; i--) {
-    //         heapify(array, array.length, i);
-    //     }
+//     // static void sort(int[] array) {
+//     //     for (int i = array.length / 2 - 1; i >= 0; i--) {
+//     //         heapify(array, array.length, i);
+//     //     }
 
-    //     for (int i = array.length - 1; i >= 0; i--) {
-    //         int temp = array[0];
-    //         array[0] = array[i];
-    //         array[i] = temp;
+//     //     for (int i = array.length - 1; i >= 0; i--) {
+//     //         int temp = array[0];
+//     //         array[0] = array[i];
+//     //         array[i] = temp;
 
-    //         heapify(array, i, 0);
-    //     }
-    // }
+//     //         heapify(array, i, 0);
+//     //     }
+//     // }
 
-    // static void heapify(int[] array, int heapSize, int rootIndex) {
-    //     int largest = rootIndex;
-    //     int leftChild = 2 * rootIndex + 1;
-    //     int rightChild = 2 * rootIndex + 2;
+//     // static void heapify(int[] array, int heapSize, int rootIndex) {
+//     //     int largest = rootIndex;
+//     //     int leftChild = 2 * rootIndex + 1;
+//     //     int rightChild = 2 * rootIndex + 2;
 
-    //     if (leftChild < heapSize && array[leftChild] > array[largest])
-    //         largest = leftChild;
+//     //     if (leftChild < heapSize && array[leftChild] > array[largest])
+//     //         largest = leftChild;
 
-    //     if (rightChild < heapSize && array[rightChild] > array[largest])
-    //         largest = rightChild;
+//     //     if (rightChild < heapSize && array[rightChild] > array[largest])
+//     //         largest = rightChild;
 
-    //     if (largest != rootIndex) {
-    //         int temp = array[rootIndex];
-    //         array[rootIndex] = array[largest];
-    //         array[largest] = temp;
+//     //     if (largest != rootIndex) {
+//     //         int temp = array[rootIndex];
+//     //         array[rootIndex] = array[largest];
+//     //         array[largest] = temp;
             
-    //         heapify(array, heapSize, largest);
-    //     }
+//     //         heapify(array, heapSize, largest);
+//     //     }
 
-    // }
-}
+//     // }
+// }
